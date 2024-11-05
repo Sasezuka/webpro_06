4daa661deddb58c779bab18124cc7df787ec2601
Sasezuka <kaosuteacher.lucky@gmail.com>
2024-11-05 10:44:52 +0900

    readmeの追加

--- app5.js
+++ app5.js
@@ -29,15 +29,20 @@ app.get("/luck", (req, res) => {
 
 app.get("/janken", (req, res) => {
   let hand = req.query.hand;
-  let win = Number( req.query.win );
-  let total = Number( req.query.total );
+  let win = Number( req.query.win ) || 0;
+  let total = Number( req.query.total ) || 0;
+
+  
   console.log( {hand, win, total});
+  
   const num = Math.floor( Math.random() * 3 + 1 );
   let cpu = '';
   if( num==1 ) cpu = 'グー';
   else if( num==2 ) cpu = 'チョキ';
   else cpu = 'パー';
+  
   // ここに勝敗の判定を入れる
+  let judgement = '';
   if (hand === 'グー' && cpu === 'チョキ' ||
     hand === 'チョキ' && cpu === 'パー' ||
     hand === 'パー' && cpu === 'グー') {
@@ -49,7 +54,7 @@ app.get("/janken", (req, res) => {
     judgement = '負け';
   }
   total += 1;
-  
+
   const display = {
     your: hand,
     cpu: cpu,
